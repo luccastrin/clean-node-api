1173f183b86b5840afcabb1496081d4b67a2a955
Lucas Trindade <luccastrin@hotmail.com>
2023-06-18 13:44:10 -0300

    ensure LoginRouter return 500 if EmailValidator throws

--- src/presentation/router/login-router.spec.ts
+++ src/presentation/router/login-router.spec.ts
@@ -17,17 +17,27 @@ const makeSut = () => {
 };
 
 const makeEmailValidator = () => {
-  class EmailValidator {
+  class EmailValidatorSpy {
     isEmailValid!: boolean;
 
     isValid(email: string) {
       return this.isEmailValid;
     }
   }
-  const emailValidatorSpy = new EmailValidator();
+  const emailValidatorSpy = new EmailValidatorSpy();
   emailValidatorSpy.isEmailValid = true;
 
   return emailValidatorSpy;
+};
+
+const makeEmailValidatorWithError = () => {
+  class EmailValidatorSpy {
+    isValid() {
+      throw new Error();
+    }
+  }
+
+  return new EmailValidatorSpy();
 };
 
 const makeAuthUseCase = () => {
@@ -214,5 +224,20 @@ describe('Login Router', () => {
     const httpResponse = await sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(500);
     expect(httpResponse.body).toEqual(new ServerError());
+  });
+
+  it('should return 500 if EmailValidator throws', async () => {
+    const authUseCaseSpy = makeAuthUseCase();
+    const emailValidatorSpy = makeEmailValidatorWithError();
+    const sut = new LoginRouter(authUseCaseSpy, emailValidatorSpy);
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      },
+    };
+    const httpResponse = await sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
   });
 });
