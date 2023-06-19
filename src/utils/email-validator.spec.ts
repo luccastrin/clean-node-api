70a3a7de530fb40b89a1cff4820ce67dac4bbf0d
Lucas Trindade <luccastrin@hotmail.com>
2023-06-19 07:19:52 -0300

    create validator class into another file

--- src/utils/email-validator.spec.ts
+++ src/utils/email-validator.spec.ts
@@ -1,18 +1,18 @@
-class EmailValidator {
-  isValid(email: string) {
-    return true;
-  }
-}
+import { EmailValidator } from './email-validator';
+
+const makeSut = () => {
+  return new EmailValidator();
+};
 
 describe('EmailValidator', () => {
   it('should return true if validator returns true', () => {
-    const sut = new EmailValidator();
+    const sut = makeSut();
     const isEmailValid = sut.isValid('valid_email@mail.com');
     expect(isEmailValid).toBe(true);
   });
 
   it('should return true if validator returns true', () => {
-    const sut = new EmailValidator();
+    const sut = makeSut();
     let isEmailValid = sut.isValid('invalid_email');
     isEmailValid = false;
     expect(isEmailValid).toBe(false);
---
+++ src/utils/email-validator.ts
@@ -0,0 +1,5 @@
+export class EmailValidator {
+  isValid(email: string) {
+    return true;
+  }
+}
