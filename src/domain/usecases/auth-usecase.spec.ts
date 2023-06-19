e3ba5638e4cc4d817b59a2027644ec1eb8e8a469
Lucas Trindade <luccastrin@hotmail.com>
2023-06-19 08:19:33 -0300

    ensure AuthUseCase calls LoadUserByEmailRepository with correct email

--- src/domain/usecases/auth-usecase.spec.ts
+++ src/domain/usecases/auth-usecase.spec.ts
@@ -1,9 +1,11 @@
 import { MissingParamError } from '../../utils/errors';
 
 class AuthUseCase {
+  constructor(private loadUserByEmailRepository?: any) {}
   async auth(email?: string, password?: string) {
     if (!email) throw new MissingParamError('email');
     if (!password) throw new MissingParamError('password');
+    await this.loadUserByEmailRepository.load(email);
   }
 }
 
@@ -18,5 +20,18 @@ describe('Auth UseCase', () => {
     const sut = new AuthUseCase();
     const promise = sut.auth('any_email@mail.com');
     expect(promise).rejects.toThrow(new MissingParamError('password'));
+  });
+
+  it('should call LoadUserByEmailRepository with correct email', async () => {
+    class LoadUserByEmailRepositorySpy {
+      email!: string;
+      async load(email: string) {
+        this.email = email;
+      }
+    }
+    const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy();
+    const sut = new AuthUseCase(loadUserByEmailRepositorySpy);
+    sut.auth('any_email@mail.com', 'any_password');
+    expect(loadUserByEmailRepositorySpy.email).toBe('any_email@mail.com');
   });
 });
